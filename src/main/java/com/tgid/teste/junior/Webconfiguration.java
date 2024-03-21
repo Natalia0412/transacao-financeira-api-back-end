2aafd401066f7fbb736d58b770964dd7fccd01e3
Natalia Barros <natalia.vieiramonteiro70@gmail.com>
2024-03-21 20:31:37 -0300

    cors arrumado

--- src/main/java/com/tgid/teste/junior/Webconfiguration.java
+++ src/main/java/com/tgid/teste/junior/Webconfiguration.java
@@ -1,17 +1,24 @@
 package com.tgid.teste.junior;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.web.method.support.HandlerMethodReturnValueHandler;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
+import java.util.List;
+
 @Configuration
 @EnableWebMvc
 public class Webconfiguration implements WebMvcConfigurer {
+    @Value("${cors.origins}")
+    private String corsOrigins;
+
     @Override
     public void addCorsMappings(CorsRegistry registry) {
-        registry.addMapping("/**")
-                .allowedOrigins("${URL-ANGULAR}") // URL do frontend Angular
+        registry.addMapping("/api/**")
+                .allowedOrigins(corsOrigins) // URL do frontend Angular
                 .allowedMethods("GET", "POST", "PUT", "DELETE")
                 .allowedHeaders("*")
                 .allowCredentials(true);
